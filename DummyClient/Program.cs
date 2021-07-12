72c8bad72b1d5d7f81d9adf9389ea1176e620fff
Oops-nana <dnjswo6253@naver.com>
2021-07-12 21:44:14 +0900

    init

---
+++ DummyClient/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace DummyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); //식당주소, 포트번호
+
+            //휴대폰 설정
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                //문지기에게 입장 문의
+                socket.Connect(endPoint); //서버가 못 받아주면 무한정 대기할 수 있음(블로킹)
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                //보낸다.
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World");
+                int sendBytes = socket.Send(sendBuff);
+
+                //보낸다.
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                //나간다.
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (Exception e)
+            {
+                e.ToString();
+            }
+
+        }
+    }
+}
---
+++ ServerCore/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //DNS
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777); //식당주소, 포트번호
+            
+            //문지기 휴대폰만들기
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp); //TCP는 소켓타입을 스트림으로 만들어줘야함
+
+            try
+            {
+                //문지기 교육
+                listenSocket.Bind(endPoint);
+
+                //영업 시작
+                //bagLog 최대 대기수
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+                    //손님 입장시키기
+                    Socket clientSocket = listenSocket.Accept();
+
+                    //받는다
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    //보낸다
+                    byte[] sencBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server");
+                    clientSocket.Send(sencBuff);
+
+                    //종료
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch(Exception e)
+            {
+                e.ToString();
+            }
+            
+        }
+    }
+}
